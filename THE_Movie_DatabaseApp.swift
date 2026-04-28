a3b9f67bff743672e78548f4c6e4d8cdd4e25741
kalyandasari14 <kalyandasari80@gmail.com>
2026-04-28 18:22:15 -0500

    Intial commit

---
+++ THE_Movie_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  THE_Movie_DatabaseApp.swift
+//  THE Movie Database
+//
+//  Created by kalyan on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct THE_Movie_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
